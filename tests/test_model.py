e014d98f8098c52d6cb57b39343828551d51886e
rakibul-islam <raakibul.uap@gmail.com>
2026-08-27 23:23:03 +0600

    Add backbone output shape test

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,4 +1,7 @@
 import numpy as np
+import torch
+
+from models.backbone import ResNetBackbone
 from utils.coordinate_transform import bounding_box_sequence_to_ego_xy, normalize_trajectory
 
 T_OBS = 10
@@ -9,3 +12,12 @@ def test_coordinate_transform_shapes():
     assert xy.shape == (T_OBS, 2)
     normed = normalize_trajectory(xy)
     assert np.allclose(normed[0], 0.0)
+
+B, T_OBS = 2, 10
+
+
+def test_backbone_output_shape():
+    backbone = ResNetBackbone(pretrained=False, freeze_stages=0, feature_dim=2048)
+    x = torch.randn(B, T_OBS, 3, 224, 224)
+    out = backbone(x)
+    assert out.shape == (B, T_OBS, 2048)
